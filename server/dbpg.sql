730d6341df0e443039a93af343ddd326a1817451
sueli.oliveira <sueli.oliveira@newconsoftware.com.br>
2024-02-23 16:43:38 -0300

    ajustes

---
+++ server/dbpg.sql
@@ -0,0 +1,14 @@
+-- Database: craft_beer
+
+-- DROP DATABASE IF EXISTS craft_beer;
+
+CREATE DATABASE craft_beer
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
